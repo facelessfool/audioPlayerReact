a29721f4d91cfb212ae79b70b632c1a2868a288f
KA CHINGSUBAM <kchings1@binghamton.edu>
2021-03-07 15:11:37 -0500

    one sample audio playable

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import logo from './logo.svg';
+import './App.css';
+import ReactPlayer from 'react-player';
+import song from  './music/song.mp3'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Audio player with react
+          <ReactPlayer
+            // url="https://file-examples-com.github.io/uploads/2017/11/file_example_MP3_700KB.mp3"
+            url={song}
+            width="400px"
+            height="50px"
+            playing={false}
+            controls={true}
+      />
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
